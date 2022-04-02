7875d8b6552e37dc712cf0eead3e59752b5959fc
Fhillip Bagsic <fhillipbagsic@Fhillips-MacBook-Pro.local>
2022-04-02 15:48:52 +0800

    Update csp

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(function (req, res, next) {
 app.use(function (req, res, next) {
   res.setHeader(
     "Content-Security-Policy",
-    "script-src 'self';style-src 'self';font-src 'self'"
+    "script-src 'self';style-src 'self';font-src 'self'; frame-ancestors 'self'"
   );
   return next();
 });
